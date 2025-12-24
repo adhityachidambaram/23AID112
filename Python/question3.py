634400c53fe4cf0a7b0286fe603d2bda6bfaaa03
Adhitya chidambaram A <adhityachidambaram@gmail.com>
2025-12-24 23:10:27 +0530

    python code for Q3

---
+++ Python/question3.py
@@ -0,0 +1,7 @@
+#Create two variables
+price = 599.99
+discount = 0.15
+
+#Calculate the Final price 
+Final_price = price - (price * discount)
+print("The final price after discount is: %.2f" % Final_price)
