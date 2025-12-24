09c4d05b7a96c9447c2b23bdaff2a467ec4b86a1
Adhitya chidambaram A <adhityachidambaram@gmail.com>
2025-12-24 23:22:50 +0530

    my code for Q4

---
+++ Python/question4.py
@@ -0,0 +1,6 @@
+#Create a variable with your favorite number
+favorite_number = int(input("Enter your favorite number: "))
+if(favorite_number %2 == 0):
+    print("Your favorite number is even.")
+else:
+    print("Your favorite number is odd.")
