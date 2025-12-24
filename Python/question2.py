b48b318f093f52a335c588b7b495db4a80affb98
Adhitya chidambaram A <adhityachidambaram@gmail.com>
2025-12-24 22:54:26 +0530

    python code for Q2

---
+++ Python/question2.py
@@ -0,0 +1,9 @@
+# Calculate the age in 2025 
+birth_year = int(input("Enter your birth year: "))
+
+# Converting into integer
+birth_year = int(birth_year)
+
+# calculating age in 2025
+age = 2025 - birth_year
+print("You will be", age ,"years old in 2025!")
