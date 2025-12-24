24b620730f779c2f1edd4d65f8333da4d9d3f293
Adhitya chidambaram A <adhityachidambaram@gmail.com>
2025-12-24 22:18:24 +0530

    Q1 code

--- Python/example1.py
+++ Python/example1.py
@@ -1,1 +1,15 @@
 # Creating different data types in Python
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+Student = input("Are you a student? (yes/no): ")
+
+# printing details
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+if Student == "yes":
+    print("Student: yes")
+else:
+    print(name, " is not a Student")
