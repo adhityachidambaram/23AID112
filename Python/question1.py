d1ece185cd4e6075ad411f455d0cf8a0e2718dfa
Adhitya chidambaram A <adhityachidambaram@gmail.com>
2025-12-24 22:37:00 +0530

    my code for Q1

--- Python/question1.py
+++ Python/question1.py
@@ -3,13 +3,10 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 height = float(input("Enter your height in meters: "))
-Student = input("Are you a student? (yes/no): ")
+are_you_student = bool(input("Are you a student? (yes/no): "))
 
 # printing details
 print("Name:", name)
 print("Age:", age)
 print("Height:", height)
-if Student == "yes":
-    print("Student: yes")
-else:
-    print(name, " is not a Student")
+print("Student:", are_you_student)
