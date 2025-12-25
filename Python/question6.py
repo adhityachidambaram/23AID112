53c39a34bbd6129ea070441b02f3eab2ecd59716
Adhitya chidambaram A <adhityachidambaram@gmail.com>
2025-12-25 09:13:17 +0530

    Determining Age

---
+++ Python/question6.py
@@ -0,0 +1,12 @@
+# Age in Integer
+age = int(input("Enter your age: "))
+
+#Output
+if age<13:
+    print("You are a child.")
+elif 13<=age<=17:
+    print("You are a teenager.")
+elif 18<=age<=64:
+    print("You are an adult.")
+else:
+    print("You are a senior")
