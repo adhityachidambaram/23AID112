3f55d784dd23d6c1299e76dade74d8280d569588
Adhitya chidambaram A <adhityachidambaram@gmail.com>
2025-12-25 13:28:03 +0530

    Temperature converter

---
+++ Python/question10.py
@@ -0,0 +1,16 @@
+#Ask user for temperature in Celsius.
+temp_in_celsius = float(input("Enter temperature in Celsius: "))
+
+#Convert to Fahrenheit (°F = °C × 9/5 + 32)
+temp = (temp_in_celsius * 9/5) + 32
+print("Temperature in Fahrenheit is:", temp)
+
+#Output messages based on temperature ranges
+if temp_in_celsius <0:
+    print("Very cold! Wear thick jacket")
+elif 0 <= temp_in_celsius <15:
+    print("Cold! Wear a jacket")
+elif 16 <= temp_in_celsius <25:
+    print("Nice weather")
+else:
+    print("Hot! Stay hydrated")
