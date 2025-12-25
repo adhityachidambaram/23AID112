68b1ac1dac8d595193b01acde138d479d6a7d9b3
Adhitya chidambaram A <adhityachidambaram@gmail.com>
2025-12-25 09:19:03 +0530

    Determining Numbers

---
+++ Python/question7.py
@@ -0,0 +1,10 @@
+# Asking the user a number
+num = int(input("Please enter a number: "))
+
+#Output
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
